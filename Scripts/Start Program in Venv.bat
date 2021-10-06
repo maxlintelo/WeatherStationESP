f54ba7ca7ac425a1d8682cf7755722d8d795e2e9
Max <max.te.lintelo@gmail.com>
2021-10-06 11:53:02 +0200

    Script to fill mock DB

--- Scripts/Start Program in Venv.bat
+++ Scripts/Start Program in Venv.bat
@@ -1,9 +1,9 @@
 @echo off
 title Script: Start Program in Venv
 cls
-set /p id=Enter name of script to start (with extension, for example: esp_4_postman_request.py): 
+set /p id=Enter name of script to start (with extension, for example: esp_4_postman_request): 
 echo Starting python venv...
-cmd /c "%~dp0..\.venv\Scripts\activate.bat & echo Starting %id%...& echo ---------- START ESP OUTPUT ---------- & py ..\%id% & deactivate"
+cmd /c "%~dp0..\.venv\Scripts\activate.bat & echo Starting %id%...& echo ---------- START ESP OUTPUT ---------- & py ..\%id%.py & deactivate"
 echo ----------- END ESP OUTPUT -----------
 echo Finished python venv.
 pause
